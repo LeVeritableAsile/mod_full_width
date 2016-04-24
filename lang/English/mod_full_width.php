14eb62a2da7add36b85df20542d9309112aed773
David Keller <david.keller@litchis.fr>
2016-04-24 20:30:36 +0200

    Initial commit.

---
+++ lang/English/mod_full_width.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_full_width = array(
+	'Use full width'				=> 'Use full width',
+	'Notice full width'				=> 'Force the forum to use all the width of the browser',
+)
+
+?>
---
+++ lang/Francais/mod_full_width.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_full_width = array(
+	'Use full width'				=> 'Occuper toute la largeur',
+	'Notice full width'				=> 'Laisse le forum occuper toute la largeur du navigateur',
+)
+
+?>
